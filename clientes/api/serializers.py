21225b9dead4f4f6ae2d224b46ae353baa957a83
kmiokande <brunnodesouzabezerra@gmail.com>
2018-12-03 12:41:17 -0300

    Update serializers.py

--- clientes/api/serializers.py
+++ clientes/api/serializers.py
@@ -5,5 +5,5 @@ from clientes.models import Cliente
 class ClienteSerializer(ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ('nome', 'sobrenome', 'data_nasc', 'nome_mae', 'cpf',
+        fields = ('id', 'nome', 'sobrenome', 'data_nasc', 'nome_mae', 'cpf',
                   'rg', 'cnh', 'telefone', 'endereco', 'data_cadastro')
